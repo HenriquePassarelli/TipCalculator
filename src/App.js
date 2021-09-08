e21ebb57a6ce89937f22c88c0b1bf04301a16d54
Henrique Passarelli <henrique.passarellicc@gmail.com>
2021-09-08 11:34:16 -0300

    tip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
-function App() {
+let Input = styled.input`
+    padding:1rem;
+    font-size:1.5rem;
+    margin: 1rem 0;
+    border-radius: 1rem;
+    outline:none;
+`
+
+function Tip() {
+
+  let [amount, setAmount] = useState(0);
+  let [tax, setTax] = useState(10);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tip Calculator</h1>
+
+      <div className="tipInput">
+        <label>How much is the bill ?</label> <br />
+        <Input type="number" value={amount} onChange={(event) => setAmount(event.target.value)} />
+        <br />
+        <label>Tip rate</label> <br />
+        <Input type="number" value={tax} onChange={(event) => setTax(event.target.value)} />
+        <hr />
+      </div>
+      {amount > 0 &&
+        <div>
+          <h3>Subtotal: R$ {amount}</h3>
+          <h3>Tax ({tax})% : </h3>
+
+          <h3 className="total">Total: R$ {Math.floor(amount * (1 + (tax / 100)))},00</h3>
+        </div>}
+
     </div>
   );
 }
 
-export default App;
+export default Tip;
